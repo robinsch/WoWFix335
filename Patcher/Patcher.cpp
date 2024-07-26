65be138e0b9cf6685e5d0f92b295fff9012ea122
robinsch <hello@robinsch.com>
2024-07-26 22:31:00 +0200

    Fixed RCE exploit

--- Patcher/Patcher.cpp
+++ Patcher/Patcher.cpp
@@ -55,6 +55,9 @@ int main(int argc, char** argv)
         return 1;
     }
 
+    // remote code execution exploit
+    write_pos<uint8_t>(0x2A7, 0xC0);
+
     // windowed mode to full screen
     write_pos<uint8_t>(0xE94, 0xEB);
 
